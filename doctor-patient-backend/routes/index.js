b0a6379004789373703498a0f1f66d60a79121b6
bharatsps <bharat.softprodigy@gmail.com>
2023-02-20 00:12:48 +0530

    index routes code updated

--- doctor-patient-backend/routes/index.js
+++ doctor-patient-backend/routes/index.js
@@ -2,6 +2,5 @@ const router = require("express").Router();
 router.use("/auth", require("./auth.route"));
 router.use("/doctor", require("./doctor.route"));
 router.use("/", require("./upload.route"));
-module.exports = router;
 router.use("/admin", require("./admin.route"));
 module.exports = router;
